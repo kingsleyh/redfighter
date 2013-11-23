b30089bac0b6ee53bdcea5b45b23d49580f398f3
Kingsley Hendrickse <kingsleyhendrickse@me.com>
2013-11-23 12:07:00 +0000

    added it for now

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Redfighter::Application.config.secret_key_base = 'c693cce4c3e239376dcf06e673fe0d10ea93566f5d2160cf3f827c70e73894ec998572496c7f0fce580f453c7eddfd18b7db3729549f076a25092f8205efde28'
